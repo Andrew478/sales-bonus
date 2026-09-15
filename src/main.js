b7896515248596c3a60654ab38f11c77d67a3356
Andrey Zhmako <mmmandreas@yandex.ru>
2026-09-15 19:14:43 +0300

    Try 06

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
    let { discount, sale_price, quantity } = purchase;
    discount = 1 - (discount / 100); // Эта формула дана в тексте задания, discount изначально процент, превращаем в коэффициент
-   return +(+sale_price.toFixed(2) * quantity * discount).toFixed(2);
+   return sale_price * quantity * discount;
 }
 
 /**
@@ -98,16 +98,17 @@ function analyzeSalesData(data, options) {
 
         // Далее: наполняем его объект данными с продажи
         seller.sales_count++; // Увеличили количество продаж на одну текущую
+        seller.revenue += purchase_record.total_amount;
 
         for(let record_item of purchase_record.items) {
             let productFromDatabase = productIndex[record_item.sku]; // отсюда мы возьмём инфу о себестоимости
-            let costPrice = +(productFromDatabase.purchase_price * record_item.quantity).toFixed(2); // Считаем себестоимость: себестоимость * количество проданных в чеке
+            let costPrice = productFromDatabase.purchase_price * record_item.quantity; // Считаем себестоимость: себестоимость * количество проданных в чеке
             
             let revenue = calculateRevenue(record_item);  // Считаем выручку revenue. Второй параметр для calculateRevenue не передаём (намёк в аргументе нижний прочерк)
-            seller.revenue += +revenue.toFixed(2); // было seller.revenue += revenue, но надо пройти через магию дробных чисел в тестах
+            //seller.revenue += revenue; 
             
             let profit = revenue - costPrice; // Считаем профит
-            seller.profit += +profit.toFixed(2);
+            seller.profit += profit;
 
             // Осталось вести учёт количества проданных товаров (группировка по признаку)
             if(!seller.products_sold[record_item.sku]) seller.products_sold[record_item.sku] = 0;
