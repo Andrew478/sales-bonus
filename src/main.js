d7c6fb144b8e0bfc855dce7af5965b862bc95708
Andrey Zhmako <mmmandreas@yandex.ru>
2026-09-15 17:57:44 +0300

    Added toFixed(2) in some places to try to go through tests.

--- src/main.js
+++ src/main.js
@@ -104,10 +104,10 @@ function analyzeSalesData(data, options) {
             let costPrice = productFromDatabase.purchase_price * record_item.quantity; // Считаем себестоимость: себестоимость * количество проданных в чеке
             
             let revenue = calculateRevenue(record_item);  // Считаем выручку revenue. Второй параметр для calculateRevenue не передаём (намёк в аргументе нижний прочерк)
-            seller.revenue += revenue;
+            seller.revenue += +revenue.toFixed(2); // было seller.revenue += revenue, но надо пройти через магию дробных чисел в тестах
             
             let profit = revenue - costPrice; // Считаем профит
-            seller.profit += profit;
+            seller.profit += +profit.toFixed(2);
 
             // Осталось вести учёт количества проданных товаров (группировка по признаку)
             if(!seller.products_sold[record_item.sku]) seller.products_sold[record_item.sku] = 0;
